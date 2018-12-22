b48d6b166993c179aed42c3dc9f3ee105a912559
Nuthhapong Patchotchai <flooka_kung@hotmail.com>
2018-12-22 12:11:28 +0700

    test add css2

--- PlantData3/settings.py
+++ PlantData3/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_adminlte',
+    'django_adminlte_theme',
+    'Farmer.apps.FarmerConfig',
+
+
 ]
 
 MIDDLEWARE = [
