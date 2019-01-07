58b3524a78e93c5203aee7b87c72f867728241d1
Nuthhapong Patchotchai <flooka_kung@hotmail.com>
2019-01-07 18:30:19 +0700

    add plant and harvest ui

--- Farmer/views.py
+++ Farmer/views.py
@@ -19,3 +19,34 @@ def EditFarmer(request):
 
 def DeleteFarmer(request):
     return render(request, 'Farmer/DeleteFarmerForm.html')
+
+
+def ViewFarmer(request):
+    return render(request, 'Farmer/ViewPlant.html')
+
+
+def EditPlant(request):
+    return render(request, 'Farmer/EditPlant.html')
+
+
+def DeletePlant(request):
+    return render(request, 'Farmer/DeletePlant.html')
+
+
+def CreatePlant(request):
+    return render(request, 'Farmer/CreatePlant.html')
+
+def ViewHarvest(request):
+    return render(request, 'Farmer/ViewHarvest.html')
+
+
+def EditHarvest(request):
+    return render(request, 'Farmer/EditHarvest.html')
+
+
+def DeleteHarvest(request):
+    return render(request, 'Farmer/DeleteHarvest.html')
+
+
+def CreateHarvest(request):
+    return render(request, 'Farmer/CreateHarvest.html')
