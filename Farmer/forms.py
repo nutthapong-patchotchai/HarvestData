eb5ff5f794a7a6884cf4e776945c4657ed9590b2
Nuthhapong Patchotchai <flooka_kung@hotmail.com>
2019-02-25 03:24:36 +0700

    can't delete Harvest and url complax

--- Farmer/forms.py
+++ Farmer/forms.py
@@ -13,3 +13,22 @@ class addFarmer(forms.ModelForm):
             'tel',
             'User_id'
         ]
+
+class addPlant(forms.ModelForm):
+    class Meta:
+        model = Plant
+        fields = [
+            'fruit_name',
+            'fruit_breed',
+            'scale',
+            'Farmer_id'
+        ]
+
+class addHarvest(forms.ModelForm):
+    class Meta:
+        model = Harvest
+        fields = [
+            'product',
+            'years',
+            'Plant_id'
+        ]
--- Farmer/views.py
+++ Farmer/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, get_object_or_404
 from Farmer.models import Farmer, Plant, Harvest
 from django.contrib.auth.decorators import login_required
-from Farmer.forms import addFarmer
+from Farmer.forms import addFarmer, addPlant, addHarvest
 from django.contrib import messages
 import sweetify
 
@@ -18,7 +18,7 @@ def ViewFarmer(request):
 
 
 @login_required
-def CreateFarmer(request, id):
+def CreateFarmer(request, Farmer_id):
     if request.method == 'POST':
         farmerform = addFarmer(request.POST)
         if farmerform.is_valid():
@@ -29,30 +29,30 @@ def CreateFarmer(request, id):
     else:
         farmerform = addFarmer()
 
-    context ={
+    createfarmerform ={
         'farmerform': farmerform,
     }
-    return render(request, 'Farmer/CreateFarmer.html', context)
+    return render(request, 'Farmer/CreateFarmer.html', createfarmerform)
 
 
 @login_required
-def EditFarmer(request, id):
-    editfarmer = get_object_or_404(Farmer, id=id)
+def EditFarmer(request, Farmer_id):
+    editfarmer = get_object_or_404(Farmer, id=Farmer_id)
     editfarmerform = addFarmer(request.POST or None, instance=editfarmer)
     if editfarmerform.is_valid():
-        editfarmer = editfarmerform.save(commit=False)
-        editfarmer.user = request.user
-        editfarmer.save()
+        editfarmers = editfarmerform.save(commit=False)
+        editfarmers.user = request.user
+        editfarmers.save()
         return redirect('/')
-    context = {
+    editfarmerforms = {
         'editfarmerform': editfarmerform,
     }
-    return render(request, 'Farmer/EditFarmer.html', context)
+    return render(request, 'Farmer/EditFarmer.html', editfarmerforms)
 
 
 @login_required
-def DeleteFarmer(request, id):
-    deletefarmer = get_object_or_404(Farmer, id=id)
+def DeleteFarmer(request, Farmer_id):
+    deletefarmer = get_object_or_404(Farmer, id=Farmer_id)
     checkuser = deletefarmer.User_id.username
 
     if request.user.username == checkuser:
@@ -72,44 +72,96 @@ def ViewPlant(request, Farmer_id):
 
 
 @login_required
-def EditPlant(request):
-    return render(request, 'Farmer/EditPlant.html')
+def EditPlant(request,Farmer_id, Plant_id):
+    viewplant = Plant.objects.filter(Farmer_id=Farmer_id)
+    editplant = get_object_or_404(Plant, id=Plant_id)
+    editplantform = addPlant(request.POST or None, instance=editplant)
+    if editplantform.is_valid():
+        editplants = editplantform.save(commit=False)
+        editplants.user = request.user
+        editplants.save()
+        return redirect('/')
+    editplantforms = {
+        'editplantform': editplantform,
+    }
+    return render(request, 'Farmer/EditPlant.html', editplantforms)
 
 
 @login_required
-def DeletePlant(request):
-    return render(request, 'Farmer/DeletePlant.html')
+def DeletePlant(request,Farmer_id,Plant_id):
+    deleteplant = get_object_or_404(Plant, id=Plant_id)
+    deleteplant.delete()
+    return redirect('/')
 
-
-@login_required
-def CreatePlant(request):
-    return render(request, 'Farmer/CreatePlant.html')
 
 
 @login_required
-def ViewHarvest(request, Farmer_id, Plant_id):
-    da = Plant.objects.filter(Farmer_id=Farmer_id)
+def CreatePlant(request, Farmer_id):
+    if request.method == 'POST':
+        plantform = addPlant(request.POST)
+        if plantform.is_valid():
+            createplant = plantform.save(commit=False)
+            createplant.user = request.user
+            createplant.save()
+            return redirect("/")
+    else:
+        plantform = addPlant()
+
+    plantcreateform ={
+        'plantform': plantform,
+    }
+    return render(request, 'Farmer/CreatePlant.html', plantcreateform)
+
+
+@login_required
+def ViewHarvest(request,Farmer_id, Plant_id):
     viewharvest = Harvest.objects.filter(Plant_id=Plant_id)
     viewharvestpage = {
-        'da': da,
         'viewharvest': viewharvest
     }
     return render(request, 'Farmer/ViewHarvest.html', viewharvestpage)
 
 
 @login_required
-def EditHarvest(request):
-    return render(request, 'Farmer/EditHarvest.html')
+def EditHarvest(request, Farmer_id, Harvest_id):
+    viewplant = Plant.objects.filter(Farmer_id=Farmer_id)
+    viewharvest = Harvest.objects.filter(Plant_id=Harvest_id)
+    editharvest = get_object_or_404(Harvest, id=Harvest_id)
+    editharvestform = addHarvest(request.POST or None, instance=editharvest)
+    if editharvestform.is_valid():
+        editharvests = editharvestform.save(commit=False)
+        editharvests.user = request.user
+        editharvests.save()
+        return redirect('/')
+    editharvestforms = {
+        'editharvestform': editharvestform,
+    }
+    return render(request, 'Farmer/EditHarvest.html', editharvestforms)
 
 
 @login_required
-def DeleteHarvest(request):
-    return render(request, 'Farmer/DeleteHarvest.html')
+def DeleteHarvest(request,Farmer_id,Harvest_id):
+    deleteHarvest = get_object_or_404(Harvest, id=Harvest_id)
+    deleteHarvest.delete()
+    return redirect('/')
 
 
 @login_required
-def CreateHarvest(request):
-    return render(request, 'Farmer/CreateHarvest.html')
+def CreateHarvest(request, Farmer_id):
+    if request.method == 'POST':
+        harvestform = addHarvest(request.POST)
+        if harvestform.is_valid():
+            createharvest = harvestform.save(commit=False)
+            createharvest.user = request.user
+            createharvest.save()
+            return redirect("/")
+    else:
+        harvestform = addHarvest()
+
+    harvestcreateform ={
+        'harvestform' : harvestform
+    }
+    return render(request, 'Farmer/CreateHarvest.html', harvestcreateform)
 
 
 @login_required
